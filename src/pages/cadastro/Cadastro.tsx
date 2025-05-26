06cff1e4bfa8834f505e7bcdc91bee1b1dd173dc
User <user@192.168.0.11>
2025-05-26 10:01:11 -0300

    Login

--- src/pages/cadastro/Cadastro.tsx
+++ src/pages/cadastro/Cadastro.tsx
@@ -13,16 +13,10 @@ function Cadastro() {
 
   const[confirmaSenha, setConfirmaSenha] = useState<string>("")
 
-  const [usuario, setUsuario] = useState<Usuario>({
-    id: 0,
-    nome: '',
-    usuario: '',
-    senha: '',
-    foto: ''
-  })
+  const [usuario, setUsuario] = useState<Usuario>({} as Usuario)
   
   useEffect(() => {
-    if (usuario.id !== 0){
+    if (usuario.id !== undefined){
       retornar()
     }
   }, [usuario])
